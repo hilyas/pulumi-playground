f0b78075e059060d3e1def36f28cd2127bf31844
hilyas <hilyas@users.noreply.github.com>
2023-08-27 12:55:55 +0100

    some refactoring

--- __main__.py
+++ __main__.py
@@ -10,61 +10,55 @@ def get_project_id():
     return project_id
 
 
-def create_buckets(buckets):
-    for i, bucket in enumerate(buckets):
-        bucket_resource = storage.Bucket(bucket["name"], location=bucket["location"])
-        pulumi.export(f"bucket_name_{i}", bucket_resource.url)
+def create_bucket(name, location):
+    bucket_resource = storage.Bucket(name, location=location)
+    pulumi.export(f"bucket_url_{name}", bucket_resource.url)
+    return bucket_resource
 
 
-def create_instances(instances):
-    for i, instance_config in enumerate(instances):
-        boot_disk = compute.InstanceBootDiskArgs(
-            initialize_params=compute.InstanceBootDiskInitializeParamsArgs(
-                image=instance_config["boot_disk"]["initialize_params"]["image"]
-            )
+def create_instance(name, zone, machine_type, boot_disk_image, network):
+    boot_disk = compute.InstanceBootDiskArgs(
+        initialize_params=compute.InstanceBootDiskInitializeParamsArgs(
+            image=boot_disk_image
         )
-        network_interfaces = [
-            compute.InstanceNetworkInterfaceArgs(network=nic["network"])
-            for nic in instance_config["network_interfaces"]
-        ]
-        instance = compute.Instance(
-            instance_config["name"],
-            zone=instance_config["zone"],
-            machine_type=instance_config["machine_type"],
-            boot_disk=boot_disk,
-            network_interfaces=network_interfaces,
-        )
-        pulumi.export(f"instance_name_{i}", instance.name)
+    )
+    network_interfaces = [compute.InstanceNetworkInterfaceArgs(network=network)]
+    instance = compute.Instance(
+        name,
+        zone=zone,
+        machine_type=machine_type,
+        boot_disk=boot_disk,
+        network_interfaces=network_interfaces,
+    )
+    pulumi.export(f"instance_name_{name}", instance.name)
+    return instance
 
 
-def create_clusters(clusters):
-    for cluster_config in clusters:
-        primary = container.Cluster(
-            cluster_config["name"],
-            location=cluster_config["location"],
-            remove_default_node_pool=cluster_config["remove_default_node_pool"],
-            initial_node_count=cluster_config["initial_node_count"],
-        )
-
-        primary_preemptible_nodes = [
-            container.NodePool(
-                cluster_config["name"] + "preemptible",
-                location=cluster_config["location"],
-                cluster=primary.name,
-                node_count=cluster_config["initial_node_count"],
-                node_config=container.NodePoolNodeConfigArgs(
-                    preemptible=node_pool["preemptible"],
-                    machine_type=node_pool["machine_type"],
-                    disk_size_gb=node_pool["disk_size_gb"],
-                    service_account=node_pool["service_account"],
-                    oauth_scopes=["https://www.googleapis.com/auth/cloud-platform"],
-                ),
-            )
-            for node_pool in cluster_config["node_pools"]
-        ]
+def create_cluster(name, location, remove_default_node_pool, initial_node_count):
+    cluster = container.Cluster(
+        name,
+        location=location,
+        remove_default_node_pool=remove_default_node_pool,
+        initial_node_count=initial_node_count,
+    )
+    return cluster
 
 
-project_id = get_project_id()
+def create_node_pool(cluster_name, location, cluster, node_count, node_config):
+    node_pool = container.NodePool(
+        cluster_name + "preemptible",
+        location=location,
+        cluster=cluster.name,
+        node_count=node_count,
+        node_config=container.NodePoolNodeConfigArgs(
+            preemptible=node_config["preemptible"],
+            machine_type=node_config["machine_type"],
+            disk_size_gb=node_config["disk_size_gb"],
+            service_account=node_config["service_account"],
+            oauth_scopes=["https://www.googleapis.com/auth/cloud-platform"],
+        ),
+    )
+    return node_pool
 
 
 def main():
@@ -75,13 +69,35 @@ def main():
     clusters = config.require_object("clusters")
 
     # Create and export the buckets
-    create_buckets(buckets)
+    for bucket in buckets:
+        create_bucket(bucket["name"], bucket["location"])
 
     # Create and export the instances
-    create_instances(instances)
+    for instance_config in instances:
+        create_instance(
+            instance_config["name"],
+            instance_config["zone"],
+            instance_config["machine_type"],
+            instance_config["boot_disk"]["initialize_params"]["image"],
+            instance_config["network_interfaces"][0]["network"],
+        )
 
-    # Create and export the cluster
-    create_clusters(clusters)
+    # Create and export the clusters and their node pools
+    for cluster_config in clusters:
+        cluster = create_cluster(
+            cluster_config["name"],
+            cluster_config["location"],
+            cluster_config["remove_default_node_pool"],
+            cluster_config["initial_node_count"],
+        )
+        for node_pool in cluster_config["node_pools"]:
+            create_node_pool(
+                cluster_config["name"],
+                cluster_config["location"],
+                cluster,
+                cluster_config["initial_node_count"],
+                node_pool,
+            )
 
 
 if __name__ == "__main__":
